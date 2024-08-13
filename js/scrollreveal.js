c6863b2e6afb24c5f80e4a076e82a7eb4af7c6be
RafaelOliverSan <153394151+RafaelOliverSan@users.noreply.github.com>
2024-08-13 13:56:23 -0300

    Update scrollreveal.js

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,50 +1,5 @@
 window.revelar = ScrollReveal({reset:true})
 
-// revelar txt-itens
-
-// revelar.reveal('.txt-itens-1',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'left'
-// })
-
-// revelar.reveal('.txt-itens-2',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'right',
-//   delay: 500
-// })
-
-// revelar.reveal('.txt-itens-3',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'left',
-//   delay: 750
-// })
-
-// // revelar img-itens
-// revelar.reveal('.img-itens-1',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'right',
-//   delay: 250
-// })
-
-// revelar.reveal('.img-itens-2',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'left',
-//   delay: 750
-
-// })
-
-// revelar.reveal('.img-itens-3',{
-//   duration: 2000,
-//   distance: '90px',
-//   origin: 'right',
-//   delay: 1000
-// })
-
 
 
 // revelar btn-contatos
